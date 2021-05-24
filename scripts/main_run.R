6f735b3a25f43be43be1082e36b48f6b7fbf9509
adamkucharski <adam.kucharski@lshtm.ac.uk>
2021-05-24 12:17:36 +0100

    Update plots

--- scripts/main_run.R
+++ scripts/main_run.R
@@ -32,7 +32,7 @@ cap_outbreak_size <- 1e4 # Cap simulations to prevent runaway epidemics
 priorScale <- function(x){ifelse(abs(x)<=1,1,0)} # Prior on relative R values for non-travellers
 
 # Fit model
-run_transmission_mcmc(MCMC.runs = 3e4) # Specify number of MCMC iterations: >1e5 recommended
+run_transmission_mcmc(MCMC.runs = 1e5) # Specify number of MCMC iterations: >1e5 recommended
 
 # Simulate & plot outputs  -------------------------------------------------------------
 
